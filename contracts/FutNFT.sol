14899dfa953e8fb267ceb590da88e13a97bbb834
anim7 <anishpandit0703@gmail.com>
2022-03-13 23:07:48 +0530

    Added IPFS

--- contracts/FutNFT.sol
+++ contracts/FutNFT.sol
@@ -5,56 +5,30 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @title FutNFT
-/// @author Anish Pandit
-/// @notice This contract is mainly for minting and getting football players
-/// @dev getPlayer, ownerOf, getPlayerExists, _beforeTokenTransfer and supportsInterface are implemented without side effects
 contract FutNFT is ERC721, ERC721Enumerable, Ownable {
-    /// @notice Player struct to store information about players
     /// @dev Should have logic for players with no suitable position array
-    /// @dev NA in suitablePositions no suitable position
     struct Player {
-        uint256 id;
         string name;
         string preferredPosition;
+        uint256 id;
         uint8 age;
         uint8 level;
         uint32 lastUpgrade;
         string[] suitablePositions;
+        string imageURI;
     }
 
-    /// @notice History struct to store owner win and loss count
     struct History {
         uint256 winCount;
         uint256 lossCount;
     }
-
-    /// @notice Stores the owner's win and loss count
-    /// @dev The key is the owner address and the value is the History (win and loss count)
     mapping(address => History) ownerHistory;
-
-    /// @notice This stores all the players
-    /// @dev the key is the player id and the value is the player
-    mapping(uint256 => Player) public players;
-
-    /// @notice This stores all the owners of the players
-    /// @dev The key is the player id and the value is the address of the owner
+    mapping(uint256 => Player) players;
     mapping(uint256 => address) public playerToOwner;
-
-    /// @notice This stores the number of player a owner has
-    /// @dev The key is the address of the owner and the value is the count of players
-    mapping(address => uint256) public ownerPlayerCount;
-
-    /// @notice This stores the players who are listed on the market
-    /// @dev The key is the player id and the value is a boolean value
     mapping(uint256 => bool) public listedOnMarket;
 
-    /// @notice This event is fired when a player is minted
-    /// @param playerId the uint id of the player
     event PlayerAdded(uint256 playerId);
 
-    /// @notice Checks if the player is owned by the sender of the message
-    /// @param id The uint id of the player
     modifier owned(uint256 id) {
         require(getPlayerExists(id), "Player does not exist");
         require(msg.sender == ownerOf(id), "Not the owner!");
@@ -63,17 +37,10 @@ contract FutNFT is ERC721, ERC721Enumerable, Ownable {
 
     constructor() ERC721("FutNFT", "FNFT") {}
 
-    /// @notice Gets a player by id
-    /// @param _id The uint id of the player
-    /// @return player The player fetched using the id
     function getPlayer(uint256 _id) public view returns (Player memory) {
         return players[_id];
     }
 
-    /// @notice Gets the owner of a player by id
-    /// @param tokenId The uint id of the player
-    /// @return ownerAddress The address of the owner of the player
-    /// @inheritdoc	ERC721
     function ownerOf(uint256 tokenId)
         public
         view
@@ -83,15 +50,10 @@ contract FutNFT is ERC721, ERC721Enumerable, Ownable {
         return super.ownerOf(tokenId);
     }
 
-    /// @notice Checks if a player exists by id
-    /// @param _id The uint id of the player
-    /// @return playerExists Boolean value which tells if the player exits or not
     function getPlayerExists(uint256 _id) public view returns (bool) {
         return playerToOwner[_id] != address(0);
     }
 
-    /// @notice Mints a player
-    /// @param _player The player of type Player which is to be minted
     function mint(Player memory _player) public onlyOwner {
         require(playerToOwner[_player.id] == address(0), "Player Exists!");
         players[_player.id] = _player;
@@ -100,10 +62,6 @@ contract FutNFT is ERC721, ERC721Enumerable, Ownable {
         emit PlayerAdded(_player.id);
     }
 
-    /// @param from The address of the account from which the token is to be transferred
-    /// @param to The address of the account to which the token is to be transferred
-    /// @param tokenId The uint id of the token
-    /// @inheritdoc	ERC721
     function _beforeTokenTransfer(
         address from,
         address to,
@@ -112,9 +70,6 @@ contract FutNFT is ERC721, ERC721Enumerable, Ownable {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    /// @param interfaceId The id of the interface of type bytes4
-    /// @return supportsInterface Boolean value
-    /// @inheritdoc	ERC721
     function supportsInterface(bytes4 interfaceId)
         public
         view
--- contracts/FutNFTTraining.sol
+++ contracts/FutNFTTraining.sol
@@ -3,23 +3,13 @@ pragma solidity ^0.8.12;
 
 import "./FutNFT.sol";
 
-/// @title FutNFTTraining
-/// @dev ADD A MAX LEVEL STATE VARIABLE TO CONTROL THE MAX LEVEL
-/// @author Anish Pandit
-/// @notice This contract can be used to train players to increase their level
 contract FutNFTTraining is FutNFT {
-    /// @notice This stores the value of the cooldown period
     uint256 public cooldown = 432000 seconds;
-    /// @notice This stores the value of the fee required to level up
     uint256 public fee = 0.001 ether;
+    uint256 public maxLevel = 100;
 
-    /// @notice This event is fired when a player is levelled up
-    /// @param playerId The uint id of the player
     event LevelUp(uint256 playerId);
 
-    /// @notice This checks if the player's level is below the maximum level
-    /// @param _level The maximum level
-    /// @param _playerId The uint id of the player
     modifier belowLevel(uint256 _level, uint256 _playerId) {
         require(
             players[_playerId].level < _level,
@@ -28,9 +18,6 @@ contract FutNFTTraining is FutNFT {
         _;
     }
 
-    /// @notice Checks if the cooldown period of the player is over
-    /// @param _playerId The uint id of the player
-    /// @param _cooldown The cooldown period
     modifier cooldownOver(uint256 _playerId, uint256 _cooldown) {
         require(
             players[_playerId].lastUpgrade + cooldown <= block.timestamp,
@@ -39,28 +26,23 @@ contract FutNFTTraining is FutNFT {
         _;
     }
 
-    /// @notice Sets the cooldown period
-    /// @dev Only the owner of the contract can do so
-    /// @param _cooldown The new cooldown period
     function setCooldown(uint256 _cooldown) external onlyOwner {
         cooldown = _cooldown;
     }
 
-    /// @notice Sets the required fee to level up
-    /// @dev Only the owner of the contract can do so
-    /// @param _fee The new fee
     function setFee(uint256 _fee) external onlyOwner {
         fee = _fee;
     }
 
-    /// @notice Trains the player
-    /// @dev The owner should send the required fee and the player should be below level 100
-    /// @param _playerId The uint id of the player who is to be trained
+    function setMaxLevel(uint256 _level) external onlyOwner {
+        maxLevel = _level;
+    }
+
     function train(uint256 _playerId)
         external
         payable
         cooldownOver(_playerId, cooldown)
-        belowLevel(100, _playerId)
+        belowLevel(maxLevel, _playerId)
         owned(_playerId)
     {
         require(msg.value >= fee, "Required fee is not sent!");
